346acff0ec1ccbf8a18b1da15e76d93720804e9d
= <=>
2024-08-09 13:49:51 +0000

    added severless function

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'index.js'));
   });
   
   app.listen(port, () => {
